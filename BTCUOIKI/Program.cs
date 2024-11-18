6a0b1693929162c8790f3fff05af538432866685
dothanhbao1805 <dothanhbao42@gmail.com>
2024-11-18 16:48:57 +0700

    bao_Update

--- BTCUOIKI/Program.cs
+++ BTCUOIKI/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BTCUOIKI.GUI;
 
 namespace BTCUOIKI
 {
@@ -16,7 +17,7 @@ namespace BTCUOIKI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DangNhap());
         }
     }
 }
